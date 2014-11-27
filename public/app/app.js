0eb187283848c4edd082279a257d3e4d9b4f3076
MichaelFriisCph-mf117 <mfriis@gmail.com>
2014-11-27 12:47:22 +0100

    view + UI for add user is added

--- public/app/app.js
+++ public/app/app.js
@@ -11,7 +11,8 @@ angular.module('myAppRename', [
   'myAppRename.view1',
   'myAppRename.view2',
   'myAppRename.view3',
-  'myAppRename.view4'
+  'myAppRename.view4',
+  'myAppRename.view5'
 ]).
 config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/view1'});
---
+++ public/app/view5/view5.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Michael on 27-11-2014.
+ */
+
+'use strict';
+
+angular.module('myAppRename.view5', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view5', {
+            templateUrl: 'app/view5/view5.html',
+            controller: 'View5Ctrl'
+        });
+    }])
+
+    .controller('View5Ctrl', function ($scope, $http) {
+
+
+    });
+
+
